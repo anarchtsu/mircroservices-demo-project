aef9670286f1aba586c0597c2a96db643c9ad296
anarchtsu <gontarenko.dev@gmail.com>
2022-01-24 22:29:37 +0300

    added new fraud-service

---
+++ fraud/src/main/java/ru/gontarenko/fraud/FraudApp.java
@@ -0,0 +1,11 @@
+package ru.gontarenko.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(FraudApp.class, args);
+    }
+}
