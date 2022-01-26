c38523ca08fe3b1326f4045ce351e2cd5ba5ff00
anarchtsu <gontarenko.dev@gmail.com>
2022-01-26 21:03:17 +0300

    added eureka-server

---
+++ eureka-server/src/main/java/ru/gontarenko/eureka/EurekaServerApp.java
@@ -0,0 +1,17 @@
+package ru.gontarenko.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * http://localhost:8761
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApp.class, args);
+    }
+}
