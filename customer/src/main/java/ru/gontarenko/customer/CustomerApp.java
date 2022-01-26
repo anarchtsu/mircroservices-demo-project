9f9be564eb2e5cc104335ebb89670d95eef108ea
anarchtsu <gontarenko.dev@gmail.com>
2022-01-26 21:25:26 +0300

    customer-service: eureka client

--- customer/src/main/java/ru/gontarenko/customer/CustomerApp.java
+++ customer/src/main/java/ru/gontarenko/customer/CustomerApp.java
@@ -2,7 +2,9 @@ package ru.gontarenko.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CustomerApp {
     public static void main(String[] args) {
