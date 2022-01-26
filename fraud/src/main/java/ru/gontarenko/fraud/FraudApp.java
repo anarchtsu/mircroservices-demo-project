7d7caf401fa5651072cfaf66b8de9fdb47de444e
anarchtsu <gontarenko.dev@gmail.com>
2022-01-26 21:24:50 +0300

    fraud-service: eureka client

--- fraud/src/main/java/ru/gontarenko/fraud/FraudApp.java
+++ fraud/src/main/java/ru/gontarenko/fraud/FraudApp.java
@@ -2,7 +2,9 @@ package ru.gontarenko.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class FraudApp {
     public static void main(String[] args) {
